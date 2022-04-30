f1311222220638f7fe940a120be77127bd1ba786
Tural Novruzov <tural.nt.1@gmail.com>
2022-04-30 19:43:43 +0300

    Added Trending Tags to fronted and  added filtering by tags

--- hipogram/posts/views.py
+++ hipogram/posts/views.py
@@ -1,11 +1,14 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Count
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 from django.views.generic.list import ListView
 from hipogram.posts.forms import CreatePostForm
+from hipogram.tags.models import Tag
 
 from .models import Post
+
 
 def post_list_view(request):
     posts = Post.objects.all().order_by("-creation_datetime")
@@ -17,6 +20,22 @@ class ListPostsView(ListView):
     ordering = '-creation_datetime'
     context_object_name = 'posts'
     paginate_by = 5
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        tag_name = self.request.GET.get('tag', None)
+        if tag_name is not None:
+            queryset = queryset.filter(tags__name=tag_name)
+        
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tags'] = Tag.objects.all() \
+                             .annotate(num_posts=Count('posts')) \
+                             .order_by('-num_posts')
+        return context
 
 class CreatePostView(LoginRequiredMixin, CreateView):
     template_name = 'post_create.html'
