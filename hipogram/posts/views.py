e6b1491117a8f2510041fed59421ce8191b13a4b
Tural Novruzov <tural.nt.1@gmail.com>
2022-04-30 19:48:06 +0300

    Added comments to post views for clarity

--- hipogram/posts/views.py
+++ hipogram/posts/views.py
@@ -24,14 +24,18 @@ class ListPostsView(ListView):
     def get_queryset(self):
         queryset = super().get_queryset()
 
+        # See if there is a tag parameter in the query
         tag_name = self.request.GET.get('tag', None)
         if tag_name is not None:
+            # Filter the posts based on whether they have the given tag
             queryset = queryset.filter(tags__name=tag_name)
         
         return queryset
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+
+        # Add all tags to the context ordered by the number of posts that have the tag descending
         context['tags'] = Tag.objects.all() \
                              .annotate(num_posts=Count('posts')) \
                              .order_by('-num_posts')
@@ -44,6 +48,7 @@ class CreatePostView(LoginRequiredMixin, CreateView):
 
     def form_valid(self, form):
         self.object = form.save(commit=False)
+        # Make the current user be the creator of the post
         self.object.created_by = self.request.user
         self.object.save()
         return super().form_valid(form)
